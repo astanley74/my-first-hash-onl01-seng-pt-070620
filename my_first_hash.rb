01bafc9e926cac213f9f3f6d91f1188cf95b3721
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-14 22:55:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,12 +30,7 @@ def adding
                  }
   shipping_manifest["muskets"] = 2
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
 
-
   shipping_method
-
 end
